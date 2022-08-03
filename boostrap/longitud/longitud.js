159b79929a0d74a3eef345b9fb517c2a620b1275
valentina <101824470+valentina1706@users.noreply.github.com>
2022-08-03 16:00:03 -0300

    Add files via upload
    listo

---
+++ boostrap/longitud/longitud.js
@@ -0,0 +1,58 @@
+console.log ("desde js");
+
+//selecciono elementos vinculados al html para procesar
+const boton = document.getElementById("boton");
+
+
+
+const milimetros = document.getElementById("milimetros")
+
+const centimetros = document.getElementById("centimetros")
+
+const decimetros = document.getElementById("decimetros")
+
+const decametro = document.getElementById("decametro")
+
+const hectometro = document.getElementById("hectometro")
+
+const kilometros = document.getElementById("kilometros")
+
+//evento click
+
+boton.addEventListener("click",metros);
+
+//crear funcion
+
+function metros() {
+
+    let metros = document.getElementById("metros").value;
+    metros = parseInt(metros);
+
+
+    //instrucciones
+    
+   milimetros.textContent=metros*1000
+
+   centimetros.textContent=metros*100
+
+   decimetros.textContent=metros*10
+
+   decametros.textContent=metros/10
+
+   hectometros.textContent=metros/100
+
+   kilometros.textContent=metros/1000
+
+
+
+
+
+
+   
+
+
+
+
+  
+   
+}
